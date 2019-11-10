2dbf56527cd661e7ce9b711bedebbbe00e032a21
Fernando Pacheco <fpacheco@users.noreply.github.com>
2019-11-10 09:05:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,11 @@ def plot_model(domain, grid):
         grid {pygridgen} -- The grid
     """
     fig, ax = plt.subplots(figsize=(7, 7), subplot_kw={'aspect':'equal'})
-    fig = pgt.viz.plotCells(grid.x, grid.y, ax=ax)
-    fig = pgt.viz.plotDomain(domain.geometry.x, domain.geometry.y, beta=domain.efdc, ax=ax)
+    # Update to las version 
+    #fig = pgt.viz.plotCells(grid.x, grid.y, ax=ax)
+    #fig = pgt.viz.plotDomain(domain.geometry.x, domain.geometry.y, beta=domain.efdc, ax=ax)
+    fig = pgt.viz.plot_cells(grid.x, grid.y, ax=ax)
+    pgt.viz.plot_domain(domain, betacol='efdc', ax=ax) 
 
 domain = gpd.read_file(POINT_MODEL_SHAPEFILE)
 grid = pgg.Gridgen(domain.geometry.x, domain.geometry.y, domain.efdc, shape=(NYDIV, NXDIV), thin=True)
